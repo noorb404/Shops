0237ed35c53ab74394ecb9b812b34c15c55a8beb
noor <noor.bakrieh@gmail.com>
2020-10-12 16:43:03 +0300

    Https/font/ngx bootstrap

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,6 +26,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +40,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
